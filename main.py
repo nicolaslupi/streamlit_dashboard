746dad52badd23692c0b423a6619b226d67ce76e
lipinoelbreve <nicolupi.2@gmail.com>
2022-12-27 12:01:44 -0300

    AUTH + Omito presupuesto cuando restrinjo a alguna ubicacion en lugar de todas

--- main.py
+++ main.py
@@ -2,66 +2,89 @@ import streamlit as st
 import utils
 import datetime as dt
 import pandas as pd
+import streamlit_authenticator as stauth
+import yaml
 
 st.set_page_config(page_title="Epic Dashboard", layout='wide', page_icon='🛰') 
 
-DATA_URL = 'https://drive.google.com/uc?export=download&id=10rbQufLi-1kvH3cIDZvWmouMiS3YYR4O'
-DATA_FILENAME = 'data.xlsx'
-TEAMS_URL = 'https://drive.google.com/uc?export=download&id=1EoHksGxR6zl9xMNsPfx7JbhDDqr36hyv'
-TEAMS_FILENAME = 'labor.xlsx'
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
 
-data, data_distr, months, quarters, cuentas = utils.load_data(url=DATA_URL, filename=DATA_FILENAME)
-data['fecha'] = data.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
-data_distr['fecha'] = data_distr.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
-months = [dt.datetime.date(month) for month in months]
-teams = utils.load_teams(url=TEAMS_URL, filename=TEAMS_FILENAME)
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
 
-refresh = st.sidebar.button('Actualizar Datos', help='Elimina Caché y actualiza datos desde Drive')
-if refresh:
-  st.experimental_memo.clear()
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+  authenticator.logout('Logout', 'sidebar')
+  
+  DATA_URL = 'https://drive.google.com/uc?export=download&id=10rbQufLi-1kvH3cIDZvWmouMiS3YYR4O'
+  DATA_FILENAME = 'data.xlsx'
+  TEAMS_URL = 'https://drive.google.com/uc?export=download&id=1EoHksGxR6zl9xMNsPfx7JbhDDqr36hyv'
+  TEAMS_FILENAME = 'labor.xlsx'
+
   data, data_distr, months, quarters, cuentas = utils.load_data(url=DATA_URL, filename=DATA_FILENAME)
   data['fecha'] = data.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
   data_distr['fecha'] = data_distr.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
   months = [dt.datetime.date(month) for month in months]
   teams = utils.load_teams(url=TEAMS_URL, filename=TEAMS_FILENAME)
 
-st.sidebar.header("Navegación")
+  refresh = st.sidebar.button('Actualizar Datos', help='Elimina Caché y actualiza datos desde Drive')
+  if refresh:
+    st.experimental_memo.clear()
+    data, data_distr, months, quarters, cuentas = utils.load_data(url=DATA_URL, filename=DATA_FILENAME)
+    data['fecha'] = data.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
+    data_distr['fecha'] = data_distr.fecha.apply(lambda fecha: dt.datetime.date(pd.to_datetime(fecha)))
+    months = [dt.datetime.date(month) for month in months]
+    teams = utils.load_teams(url=TEAMS_URL, filename=TEAMS_FILENAME)
 
-pagina = st.sidebar.radio('', options=['Caja','Gastos','Aportes','Equipo'], index=0)
-st.sidebar.subheader("Parámetros generales")
+  st.sidebar.header("Navegación")
 
-moneda = st.sidebar.selectbox(
-  'Moneda',
-  ['USD','ARS']
-)
+  pagina = st.sidebar.radio('', options=['Caja','Gastos','Aportes','Equipo'], index=0)
+  st.sidebar.subheader("Parámetros generales")
 
-sites = list(set(data.site))
-sites = st.sidebar.multiselect(
-  'Empresa',
-  sites,
-  sites
-)
+  moneda = st.sidebar.selectbox(
+    'Moneda',
+    ['USD','ARS']
+  )
 
-devengado = st.sidebar.checkbox(label='Devengado', help='Distribuye gastos en el tiempo cuando corresponde. Por ejemplo, el caso de un depósito por un alquiler. No son números reales.')
+  sites = list(set(data.site))
+  sites = st.sidebar.multiselect(
+    'Empresa',
+    sites,
+    sites
+  )
 
-if devengado:
-  data_aux, flow, stock = utils.filter(data_distr, sites, moneda.lower())
-else:
-  data_aux, flow, stock = utils.filter(data, sites, moneda.lower())
-  
-if pagina == 'Caja':
-  st.title("Resumen de Cuentas")
-  st.subheader('Estado de Caja')
-  utils.caja(data_aux, flow, stock, moneda.lower())
-elif pagina == 'Gastos':
-  st.title('Reporte de Gastos')
-  date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(dt.date(year=2021, month=1, day=1), data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
-  utils.gastos(data_aux, flow, moneda.lower(), date_range)
-elif pagina == 'Aportes':
-  st.title('Aportes')
-  date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(months[0], data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
-  utils.aportes(data_aux, moneda.lower(), date_range)
-else:
-  st.title('Equipo')
-  date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(months[0], data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
-  utils.equipo(data_aux, teams, moneda.lower(), date_range)
+  devengado = st.sidebar.checkbox(label='Devengado', help='Distribuye gastos en el tiempo cuando corresponde. Por ejemplo, el caso de un depósito por un alquiler. No son números reales.')
+
+  if devengado:
+    data_aux, flow, stock = utils.filter(data_distr, sites, moneda.lower())
+  else:
+    data_aux, flow, stock = utils.filter(data, sites, moneda.lower())
+    
+  if pagina == 'Caja':
+    st.title("Resumen de Cuentas")
+    st.subheader('Estado de Caja')
+    utils.caja(data_aux, flow, stock, moneda.lower())
+  elif pagina == 'Gastos':
+    st.title('Reporte de Gastos')
+    date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(dt.date(year=2021, month=1, day=1), data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
+    utils.gastos(data_aux, flow, moneda.lower(), date_range)
+  elif pagina == 'Aportes':
+    st.title('Aportes')
+    date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(months[0], data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
+    utils.aportes(data_aux, moneda.lower(), date_range)
+  else:
+    st.title('Equipo')
+    date_range = st.sidebar.date_input('Rango de fechas', min_value=months[0], value=(months[0], data_aux.fecha.iloc[-1]), max_value=data_aux.fecha.values[-1])
+    utils.equipo(data_aux, teams, moneda.lower(), date_range)
+
+
+elif authentication_status == False:
+  st.error('Username/password is incorrect')
+elif authentication_status == None:
+  st.error('Please enter your username and password')
